09699191a957b01de688c931411bcbb64a44a30d
MANTISKYU <asdaron44@gmail.com>
2024-05-22 14:41:21 +0900

    create divide function

--- src/main/java/com/eyesofsol/calculator/Application.java
+++ src/main/java/com/eyesofsol/calculator/Application.java
@@ -2,4 +2,18 @@ package com.eyesofsol.calculator;
 
 public class Application {
 
+
+    public static void main(String[] args) {
+
+        Lee lee = new Lee();
+//        lee.divide();
+//        lee.Rest();
+//        lee.divide2();
+        lee.divide3();
+
+
+
+
+    }
+
 }
