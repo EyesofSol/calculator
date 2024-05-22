c06bfaba193946b1f7e64a177beb3ad462b74306
juyeon99 <beive999@naver.com>
2024-05-22 15:10:27 +0900

    Create calculateAverage function

--- src/main/java/com/eyesofsol/calculator/Hong.java
+++ src/main/java/com/eyesofsol/calculator/Hong.java
@@ -28,4 +28,21 @@ public class Hong {
             System.out.println(n1 + "의 " + n2 + "제곱은 " + result + "입니다.");
         }
     }
+
+    public void calculateAverage(){
+        Scanner sc = new Scanner(System.in);
+        double sum = 0;
+        int count = 0;
+        while(true){
+            System.out.print("숫자를 입력하세요. (멈추려면 'exit'): ");
+            String str1 = sc.nextLine();
+            if(str1.equals("exit")){
+                break;
+            } else{
+                sum += Double.parseDouble(str1);
+                count++;
+            }
+        }
+        System.out.println("평균값: " + sum/count);
+    }
 }
