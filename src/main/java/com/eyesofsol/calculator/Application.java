3b76d90eb4dd2979b08aef971e10c14d49f2e240
kimkinghyeon <modan8199@gmail.com>
2024-05-22 14:16:52 +0900

    class 명 : kim

--- src/main/java/com/eyesofsol/calculator/Application.java
+++ src/main/java/com/eyesofsol/calculator/Application.java
@@ -1,5 +1,12 @@
 package com.eyesofsol.calculator;
 
 public class Application {
+    public static void main(String[] args) {
+    Kim kim = new Kim();
+
+    kim.plusMinus();
+    kim.plusMinus2();
+    kim.Calculator();
+    }
 
 }
