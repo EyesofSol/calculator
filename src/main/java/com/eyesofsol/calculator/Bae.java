a0a296f0cb55ba5dde928bde8c695f24c7dd615d
HEun0420 <usialeta@gmail.com>
2024-05-22 15:02:00 +0900

    multiple

--- src/main/java/com/eyesofsol/calculator/Bae.java
+++ src/main/java/com/eyesofsol/calculator/Bae.java
@@ -9,13 +9,17 @@ public class Bae {
     public void multiple() {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("첫 번째 수를 입력해주세요 : ");
-        int n1 = sc.nextInt();
-        System.out.print("두 번째 수를 입력해주세요 : ");
-        int n2 = sc.nextInt();
+        System.out.println("첫 번째 수를 입력해주세요 : ");
+        double n1 = sc.nextDouble();
 
-        int result = n1 * n2;
-        System.out.println(n1 + " 곱하기 " + n2 + "의 결과는 " + result+ "입니다.");
-    }
+        System.out.println("두 번째 수를 입력해주세요 : ");
+        double n2 = sc.nextDouble();
 
+
+        double result = n1 * n2;
+
+            System.out.println(n1 +" 곱하기 "+n2 +"의 결과는 "+result +"입니다.");
+
+
+}
 }
